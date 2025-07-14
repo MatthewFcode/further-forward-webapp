fd5a6ece478af60c549392ce5c338ab72efde4a1
Matthew Foley <matthewfoley333@gmail.com>
2025-07-14 18:18:43 +1200

    work on blog

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ROTATING IMAGE- HOME PAGE
 // create a current index for the current image on
 // create an array of all the images
 // function that changes the index up one after a certain period of time
@@ -18,3 +19,39 @@ function imgSwap() {
 }
 
 imgSwap()
+// animating the page when entered
+window.addEventListener('DOMContentLoaded', () => {
+  let fadeClass = document.querySelectorAll('.fade-in-on-load')
+  fadeClass.forEach((el) => {
+    el.classList.add('visible')
+  })
+})
+
+//WHACKING THE HAT
+
+let cellArr = Array.from(document.getElementsByTagName('td'))
+console.log(cellArr)
+
+function chooseRandCell() {
+  let randomCell = getRandNum(cellArr)
+  console.log(randomCell)
+  let img = document.createElement('img')
+  img.src = ''
+  img.alt = 'Hat'
+  img.width = 80
+  img.height = 80
+  img.onclick = function (event) {
+    event.stopPropagation()
+    img.remove()
+  }
+  cellArr.onclick = () => {
+    randomCell.appendChild(img)
+  }
+}
+chooseRandCell()
+
+//util function
+function getRandNum(arr) {
+  let randNum = Math.floor(Math.random() * arr.length)
+  return randNum
+}
